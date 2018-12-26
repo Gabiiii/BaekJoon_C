910e9e4da4529a457201128833b56e304f109f5b
Gabiiii <anna9570@gmail.com>
2018-12-26 09:41:09 +0900

    9498 시험 성적

---
+++ 9498.c
@@ -0,0 +1,11 @@
+// 9498 시험 성적
+#include <stdio.h>
+void main() {
+	int input;
+	scanf("%d", &input);
+	if (input >= 90 && input <=100)	printf("A");
+	else if (input >= 80 && input <= 89)	printf("B");
+	else if (input >= 70 && input <= 79)	printf("C");
+	else if (input >= 60 && input <= 69)	printf("D");
+	else	printf("F");
+}
