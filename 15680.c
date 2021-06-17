25d4a0137ccdd30e3e1bcfdee13007cc567e5d82
Gabiiii <anna9570@gmail.com>
2021-06-17 14:42:43 +0900

    15680 연세대학교

---
+++ 15680.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int inp;
+    scanf("%d", &inp);
+    if(inp==0)  printf("YONSEI");
+    else if(inp==1)  printf("Leading the Way to the Future");
+    
+    return 0;
+}
